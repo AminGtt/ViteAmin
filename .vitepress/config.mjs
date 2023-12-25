94e78b35f1e88d5dc1b2584276e38c8c6645939c
Amin Gatta <gatta.amin38@gmail.com>
2023-12-25 15:41:02 +0100

    Update basepath

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/ViteAmin/',
+  base: '/',
   head: [['link', { rel: 'icon', href: '/ViteAmin/img/my-avatar.png' }]],
   title: "ViteAmin",
   description: "My VitePress portfolio",
