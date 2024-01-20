2ddeb15a6ddcb31b1021ed81019dcf4489fdf0b3
Amin Gatta <gatta.amin38@gmail.com>
2024-01-20 01:42:07 +0100

    addings

---
+++ components/SoftwareCard.vue
@@ -0,0 +1,94 @@
+<script setup>
+
+const props = defineProps(['wanted'])
+
+const mac = [
+    'AppCleaner',
+    'Arc',
+    'BatteryBoi',
+    'Brandy',
+    'Fanny',
+    'HiddenBar',
+    'iTerm2',
+    'Jiffy',
+    'KeyboardCleanTool',
+    'Maccy',
+    'MenubarX',
+    'Rectangle',
+    'The Unarchiver',
+    'Warp',
+    'Wineskin winery',
+    'xbar',
+], 
+win = [
+    'Steam',
+    'Battle.net',
+], 
+linux = [
+    'nmap',
+    'EmuDeck',
+    'WineTricks',
+],
+other = [
+    'VSCode',
+    'Jetbrains suite',
+    'Fleet',
+    'Android studio',
+    'XCode',
+    'Docker',
+    'mpv',
+    'qBitTorrent',
+    'Jellyfin',
+    'Prowlarr',
+    'Radarr',
+    'Sonarr',
+    'Lidarr',
+    'Discord',
+    'Bitwarden',
+    'Deezer',
+    'Brave',
+    'Figma',
+]
+
+let wantedList;
+if (props.wanted == "mac") {
+    wantedList = mac
+} else if (props.wanted == "win") {
+    wantedList = win
+} else if (props.wanted == "linux") {
+    wantedList = linux
+} else if (props.wanted == "other") {
+    wantedList = other
+}
+
+</script>
+
+<template>
+
+    <section>
+        <article class="software" v-for="soft in wantedList">
+            <img class="software-logo" :src="`/img/softwares-logo/`+wanted+'/'+soft+`.png`" :alt="soft+` logo`">
+            <p>
+                {{ soft }}
+            </p>
+        </article>
+    </section>
+
+    
+</template>
+
+<style scoped lang="sass">
+    section
+        display: flex
+        flex-wrap: wrap
+        justify-content: flext-start
+    .software
+        width: 172px
+        text-align: center
+
+        .software-logo
+            margin: auto
+            height: 100px
+            // width: 100px
+            border-radius: 25px
+</style>
